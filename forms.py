07c8bcb68b63a14601a4451a548eac7c7c5488e3
IDGS-803-21000008 <davidfigueroa0777@gmail.com>
2024-02-13 18:36:58 -0600

    DAFR 13/02/2024 Validacion campos y archivos txt

--- forms.py
+++ forms.py
@@ -1,9 +1,23 @@
 from wtforms import Form
 from wtforms import StringField, SelectField, RadioField, EmailField, IntegerField
+from wtforms import validators
 
 class UserForm(Form):
-    nombre = StringField('nombre')
-    apaterno = StringField('apaterno')
-    amaterno = StringField('amaterno')
-    edad = IntegerField('edad')
-    email = EmailField('email')
+    nombre = StringField('nombre', [
+        validators.DataRequired(message='el campo es requerido'),
+        validators.length(min=4, max=10, message='ingresa un nombre valido')
+    ])
+    apaterno = StringField('apaterno',[
+        validators.DataRequired(message='el campo es requerido'),
+        validators.length(min=4, max=10, message='ingresa un nombre valido')
+    ])
+    amaterno = StringField('amaterno',[
+        validators.DataRequired(message='el campo es requerido'),
+        validators.length(min=4, max=10, message='ingresa un nombre valido')
+    ])
+    edad = IntegerField('edad', [
+       validators.number_range(min=1, max=20, message='valor no valido')
+    ])
+    email = EmailField('email',[
+        validators.Email(message='Ingrese un correo válido')
+    ])
